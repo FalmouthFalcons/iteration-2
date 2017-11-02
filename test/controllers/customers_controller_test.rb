a4f653191261e2cff7fa669b4d2d312b890a6d9b
Jim Vickery <jimrv8@yahoo.com>
2017-11-02 16:32:21 -0500

    controller testing

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,19 +1,57 @@
 require 'minitest/autorun'
-require_relative '../app/controllers/customers_controller.rb'
+require_relative '../../app/controllers/customers_controller.rb'
 
 class CustomersControllerTest < Minitest::Test
-    
 
-    # Test GET all customers - get
+    def setup
+        @customersController = CustomersController.new
+        @customersController.last_name = "Anderson"
+        @customersController.first_name = "John"
+        @customersController.phone_number = "123-234-3333"
+        @customersController.street_address = "123 Elm"
+        @customersController.city = "Nashville"
+        @customersController.us_state = "TN"
+        @customersController.zip_code = "23456"
+
+        
+    end
 
 
-    # Test GET single customer - get
+    def test_add_customer_last_name
+        assert_equal @customersController.last_name,  "Anderson"
+    end
+
+    def test_add_customer_first_name
+        assert_equal @customersController.first_name,  "John"
+    end
+
+    def test_add_customer_phone_number
+        assert_equal @customersController.phone_number,  "123-234-3333"
+    end
 
 
-    # Test Create customer - post
+    def test_add_customer_street_address
+        assert_equal @customersController.street_address, "123 Elm"
+    end
 
 
-    # Test Update customers by id - patch/put
+    def test_add_customer_city
+        assert_equal @customersController.city,  "Nashville"
+    end
 
+    def test_add_customer_us_state
+        assert_equal @customersController.us_state,  "TN"
+    end
+
+    def test_add_customer_zip_code
+        assert_equal @customersController.zip_code,  "23456"
+    end
+
+
+    def test_add_new_customer
+        new_customer = @customersController.add_new_customer
+        assert_operator(new_customer, :>, 0)
+        
+    end
 
 end
