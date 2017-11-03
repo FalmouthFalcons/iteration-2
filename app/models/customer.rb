053a788e30df98ae733838cc7ffc090bef290e78
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-11-03 10:12:02 -0500

    added exception handling to all customer files. added readme template

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -21,46 +21,46 @@ class Customer
     # create customer sql
     
     def create_new_customer  
-        # begin
+        begin
         db = SQLite3::Database.open("../../db/bangazon_sqlite3.sqlite3")
         db.execute('INSERT INTO customers (last_name, first_name, phone_number, street_address, city, us_state, zip_code) VALUES ("Doe", "John", "(555)555-1234", "1234 Main Street", "Nashville", "TN", "65432")')
         last_record = db.last_insert_row_id
         db.close 
-    # rescue SQLite3::Exception => e 
-    #     p "fail to create new customer! #{e}" 
-    #     end 
         last_record
+        rescue SQLite3::Exception => e
+        p "Exception with database query: #{e}"
+        end
     end
 
     # query for all customers
 
     def get_all_customers
-        # begin
+            begin
             db = SQLite3::Database.open("../../db/bangazon_sqlite3.sqlite3")
             array_of_customers = db.execute ("SELECT * FROM customers")
             db.close 
-        #     rescue SQLite3::Exception => e 
-        #     p "fail to get all customers! #{e}" 
-        # end
+            rescue SQLite3::Exception => e 
+            p "fail to get all customers! #{e}" 
+            end
         array_of_customers
     end 
 
     # query single customer
     def get_single_customer(single_customer)
-        # begin
+            begin
             db = SQLite3::Database.open("../../db/bangazon_sqlite3.sqlite3")
             get_single = db.execute ("SELECT * FROM customers WHERE id = #{single_customer}")
             db.close 
-            # rescue SQLite3::Exception => e 
-        #     p "fail to get single customer! #{e}" 
-        # end 
+            rescue SQLite3::Exception => e 
+            p "fail to get single customer! #{e}" 
+            end 
         get_single
     end 
 
     
     # Update customer
     def update_customer(single_customer)
-        # begin
+            begin
             db = SQLite3::Database.open("../../db/bangazon_sqlite3.sqlite3")
             update_customer = db.execute(
             "UPDATE customers SET 
@@ -76,9 +76,9 @@ class Customer
             WHERE id = #{single_customer.id}")
 
             db.close 
-        #     rescue SQLite3::Exception => e 
-        #     p "fail to get update customer! #{e}" 
-        # end 
+            rescue SQLite3::Exception => e 
+            p "fail to get update customer! #{e}" 
+            end 
         update_customer
     end  
 
