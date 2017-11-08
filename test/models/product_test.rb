ee3336390ad197e5986d85606d6ee79b7fddd48d
Malcolm Ford <malcolmf38@gmail.com>
2017-11-07 20:54:35 -0600

    Still working on it

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -11,25 +11,25 @@ class ProductTest < Minitest::Test
         end
         # Test create product sql
         # I am testing that the product was created(I think, but maybe not)
-        def test_create_new_product
-                new_product_id = @product.create_new_product
-                puts new_product_id
-                assert_operator(new_product_id, :>, 0)
-        end
+        # def test_create_new_product
+        #         new_product_id = @product.create_new_product
+        #         puts new_product_id
+        #         assert_operator(new_product_id, :>, 0)
+        # end
 
 
          # Test for initialized class with arguments
-         def test_initialize_with_arguments
-                assert_instance_of Product, Product.new("arg1", "arg2")
-        end
+        #  def test_initialize_with_arguments
+        #         assert_instance_of Product, Product.new("arg1", "arg2")
+        # end
 
 
         #test for deleting product
-        def test_destroy_product
-                new_product_id = @product.create_new_product
-                destroy_product = @product.destroy_product(new_product_id)
-                assert_equal 0, destroy_product.length
-        end
+        # def test_destroy_product
+        #         new_product_id = @product.create_new_product
+        #         destroy_product = @product.destroy_product(new_product_id)
+        #         assert_equal 0, destroy_product.length
+        # end
 
         # And when one is selected, it should be deleted from the system if it has not been added to an order yet
 
@@ -39,22 +39,18 @@ class ProductTest < Minitest::Test
 
         # Test create product sql
         # I am testing that the product was created(I think, but maybe not)
-        def test_create_new_product
-                new_product_id = @product.create_new_product
-                puts new_product_id
-                assert_operator(new_product_id, :>, 0)
-        end
+        # def test_create_new_product
+        #         new_product_id = @product.create_new_product
+        #         puts new_product_id
+        #         assert_operator(new_product_id, :>, 0)
+        # end
     
         # Test query for all product
         # I am testing to see if the result is greater than or equal to one. If so, that means that I'm pulling in
         # the customer's products
-        def get_all_active_customer_products()
-                begin
-                        @active_customer_products = @product.get_all_active_customer_products
-                        assert_instance_of(Array, all_products)1er
-                rescue SQLite3::Exception => e
-                        p "Exception with test_all_customers: #{e}"
-                end
+        def test_get_all_active_customer_products()
+                @active_customer_products = @product.get_all_active_customer_products
+                assert_instance_of(Array, @active_customer_products)
         end
     
         # Test query single product
